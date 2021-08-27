af40ca60dc9171e730cc9c81009a0635fc1feab8
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2021-08-27 21:44:55 +0530

    Initial Commit

---
+++ routes/pool.js
@@ -0,0 +1,12 @@
+var mysql=require ("mysql")
+var pool =mysql.createPool({
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'123@1',
+    database:'learningmanagement',
+    connectionLimit:100,
+    multipleStatements:true
+})
+
+module.exports=pool
